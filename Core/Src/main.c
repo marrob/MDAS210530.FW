05e37aa6ae8c7de50b24dfc72d5f85f3294f001b
Margit Robert <marrob50hz@gmail.com>
2021-06-05 23:28:28 +0200

    ADC is working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,6 +31,19 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+
+typedef  struct _AdcRawChannelsTypeDef{
+  uint16_t ain0;
+  uint16_t ain1;
+  uint16_t ain2;
+  uint16_t ain3;
+  uint16_t ain4;
+  uint16_t ain5;
+  uint16_t ain6;
+}AdcRawChannelsTypeDef;
+
+uint32_t adctemp[7];
+
 typedef struct _AppTypeDef
 {
 
@@ -57,6 +70,7 @@ typedef struct _AppTypeDef
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
 
 UART_HandleTypeDef huart1;
 
@@ -64,18 +78,22 @@ UART_HandleTypeDef huart1;
 LiveLED_HnadleTypeDef hLiveLed;
 uint32_t UartRxTimestamp;
 DeviceTypeDef     Device;
-
+AdcRawChannelsTypeDef   AdcResult;
+uint16_t AD_RES = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 void LiveLedOff(void);
 void LiveLedOn(void);
 void MV205Enable(void);
+uint32_t AdcGetValue(void);
+
 
 
 
@@ -118,6 +136,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
@@ -141,17 +160,43 @@ int main(void)
 UartRxTimestamp = HAL_GetTick();
 
 
+
+
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    static uint32_t timestamp=0;
 
-    uint32_t timestamp = HAL_GetTick();
     LiveLedTask(&hLiveLed);
 
-    Device.Status.MainCycleTime = HAL_GetTick() - timestamp;
+    //Device.Status.MainCycleTime = HAL_GetTick() - timestamp;
+
+
+    if(HAL_GetTick() - timestamp > 1000){
+      timestamp = HAL_GetTick();
+
+     // HAL_ADC_Start_DMA(&hadc1, &AD_RES, 1);
+      HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&AdcResult, 7);
+
+     // DeviceDbgLog("ADC result: 0x%lX", AdcGetValue());
+
+   if(HAL_GPIO_ReadPin(LOCK1_GPIO_Port,LOCK1_Pin ) == GPIO_PIN_SET)
+     HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+   else
+     HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+
+
+   if(HAL_GPIO_ReadPin(LOCK2_GPIO_Port, LOCK2_Pin ) == GPIO_PIN_SET)
+       HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+     else
+       HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
+    }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -224,12 +269,12 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
   hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 7;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
@@ -239,6 +284,54 @@ static void MX_ADC1_Init(void)
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_2;
+  sConfig.Rank = ADC_REGULAR_RANK_3;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_3;
+  sConfig.Rank = ADC_REGULAR_RANK_4;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Rank = ADC_REGULAR_RANK_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = ADC_REGULAR_RANK_6;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_6;
+  sConfig.Rank = ADC_REGULAR_RANK_7;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -283,6 +376,22 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -318,12 +427,10 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-    // Conversion Complete & DMA Transfer Complete As Well
-    // So The AD_RES Is Now Updated & Let's Move IT To The PWM CCR1
-    // Update The PWM Duty Cycle With Latest ADC Conversion Result
-   // TIM2->CCR1 = (AD_RES<<4);
+  DeviceDbgLog("HAL_ADC_ConvCpltCallback");
 }
 
 /* printf -------------------------------------------------------------------*/
@@ -348,7 +455,33 @@ int _write(int file, char *ptr, int len)
 //}
 
 
+/* printf -------------------------------------------------------------------*/
 
+uint32_t AdcGetValue(void)
+{
+//  ADC_ChannelConfTypeDef   sConfig;
+//  sConfig.Channel      = ADC_CHANNEL_3;
+//  sConfig.Rank         = ADC_REGULAR_RANK_1;
+//  sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;
+//
+//  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+//  {
+//    DeviceErrLog("HAL_ADC_ConfigChannel.");
+//  }
+
+  if(HAL_ADC_Start(&hadc1) != HAL_OK)
+    DeviceErrLog("HAL_ADC_Start Fail");
+
+  if(HAL_ADC_PollForConversion(&hadc1, 10) != HAL_OK)
+    DeviceErrLog("HAL_ADC_PollForConversion Fail");
+
+  uint32_t value = HAL_ADC_GetValue(&hadc1);
+
+  if(HAL_ADC_Stop(&hadc1) != HAL_OK)
+    DeviceErrLog("HAL_ADC_PollForConversion Fail");
+
+  return value;
+}
 
 
 
@@ -366,9 +499,9 @@ void LiveLedOff(void)
 
 void MV205Enable(void){
 
-  HAL_GPIO_WritePin(MV205_1_EN_GPIO_Port, MV205_1_EN_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(MV205_2_EN_GPIO_Port, MV205_2_EN_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(MV205_1_EN_GPIO_Port, MV205_1_EN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(MV205_2_EN_GPIO_Port, MV205_2_EN_Pin, GPIO_PIN_RESET);
+ // HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
 
 }
 
